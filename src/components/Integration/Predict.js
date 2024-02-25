85ac1274fb9bdcc621d3cf8f593b2e9485d85df0
Tarun-314 <6213111566f@gmail.com>
2024-02-25 17:59:34 +0530

    100% Done

--- src/components/Integration/Predict.js
+++ src/components/Integration/Predict.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Conjunctiva from "./Conjunctiva";
 import FingerNail from "./FingerNail";
 import Palm from "./Palm";
